c81da1c89c63492182c437c8149a1dbe5ba72394
W. Joe Meese <meese022@umn.edu>
2020-08-24 14:54:23 -0500

    Build the Ising Hamiltonian
    Still a work in progress. Eventually I need to add the libraries
    properly in cmake. Right now the neighbor array is defined for
    a periodic square grid in two dimensions. In principle this should
    be generalized for more complex grids, but for now it should be
    fine.

--- src/Hamiltonians/Ising2d/ising2d_hamiltonian.hpp
+++ src/Hamiltonians/Ising2d/ising2d_hamiltonian.hpp
@@ -7,6 +7,65 @@
 // Include the observables enum class
 #include "ising2d_observables.hpp"
 
+// Include the grid setup
+#include "grid_setup.hpp"
+
+// Create a namespace to house the 
+// parameters.
+namespace Ising2d_Parameters
+{
+    // Hamiltonian parameters
+    constexpr size_t L = SYSTEM_SIZE_L;
+    constexpr size_t N = L * L;
+    constexpr float J = SYSTEM_EXCHANGE_J;
+    constexpr float h = SYSTEM_FIELD_H;
+    constexpr size_t num_neighbors_i = SYSTEM_NUM_NEAREST_NEIGHBORS_int;
+    constexpr float num_neighbors_f = SYSTEM_NUM_NEAREST_NEIGHBORS_float; 
+
+    // Wang Landau energy parameters
+    constexpr float energy_min = SYSTEM_ENERGY_MIN;
+    constexpr float energy_max = SYSTEM_ENERGY_MAX;
+    constexpr float energy_bin_size = SYSTEM_ENERGY_BIN_WIDTH;
+    constexpr size_t num_bins = SYSTEM_NUM_BINS;
+}
+
+template<typename data_t>
+struct State
+{
+    float energy = 0;
+    data_t magnetization = 0;
+};
+
+template<typename data_t>
+struct Ising2d
+{
+    State<data_t> current_state;
+
+    float * spin_array = nullptr;
+    size_t * neighbor_array = nullptr;
+
+    Ising2d()
+    {
+        // Initialize all spins to 1.
+        // TODO: change this to be randomized?
+        for ( size_t idx = 0; idx != Ising2d_Parameters::N; ++idx )
+            spin_array[idx] = 1.;
+
+        // TODO: Generalize this to different types 
+        // of grids.
+        define_2d_square_periodic_neighbors(Ising2d_Parameters::L,
+                                            Ising2d_Parameters::L,
+                                            Ising2d_Parameters::num_neighbors_i,
+                                            neighbor_array);
+    }
+
+    ~Ising2d()
+    {
+       if ( spin_array != nullptr ) delete [] spin_array;
+       if ( neighbor_array != nullptr ) delete [] neighbor_array;
+    }
+};
+
 
 #endif
 
--- src/Hamiltonians/Ising2d/ising2d_parameters.cxx
+++ src/Hamiltonians/Ising2d/ising2d_parameters.cxx
@@ -18,4 +18,20 @@ constexpr float SYSTEM_EXCHANGE_J = 1.0;
 // External field strength
 constexpr float SYSTEM_FIELD_H = 0.;
 
+// Number of nearest neighbors
+constexpr size_t SYSTEM_NUM_NEAREST_NEIGHBORS_int = 4;
+constexpr float SYSTEM_NUM_NEAREST_NEIGHBORS_float = 4;
+
+// Wang Landau Energy Minimum
+constexpr float SYSTEM_ENERGY_MIN = -0.5 * SYSTEM_EXCHANGE_J * SYSTEM_NUM_NEAREST_NEIGHBORS_float * SYSTEM_SIZE_L * SYSTEM_SIZE_L;
+
+// Wang Landau Energy Maximum
+constexpr float SYSTEM_ENERGY_MAX = 0.;
+
+// Wang Landau Bin Width
+constexpr float SYSTEM_ENERGY_BIN_WIDTH = SYSTEM_EXCHANGE_J * SYSTEM_NUM_NEAREST_NEIGHBORS_float; 
+
+// Wang Landau Bin Number
+constexpr size_t SYSTEM_NUM_BINS = static_cast<size_t>( (SYSTEM_ENERGY_MAX - SYSTEM_ENERGY_MIN) / SYSTEM_ENERGY_BIN_WIDTH );
+
 #endif
