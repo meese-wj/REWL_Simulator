c6ae366d97ce56cf0b12d588f3df7d20ce4c8362
W. Joe Meese <meese022@umn.edu>
2020-08-25 04:54:12 -0500

    Add Hamiltonian-specific functions
    These are written in a fairly general form to help later with the
    templates in the REWL updates.

--- src/Hamiltonians/Ising2d/ising2d_hamiltonian.hpp
+++ src/Hamiltonians/Ising2d/ising2d_hamiltonian.hpp
@@ -7,8 +7,9 @@
 // Include the observables enum class
 #include "ising2d_observables.hpp"
 
-// Include the grid setup
-#include "grid_setup.hpp"
+// Include the grid setup from the 
+// cmake include directories.
+#include <grid_setup.hpp>
 
 // Create a namespace to house the 
 // parameters.
@@ -29,11 +30,14 @@ namespace Ising2d_Parameters
     constexpr size_t num_bins = SYSTEM_NUM_BINS;
 }
 
+// TODO: Upgrade the energies to allow for 
+// double calculations. 
 template<typename data_t>
 struct State
 {
     float energy = 0;
     data_t magnetization = 0;
+    data_t DoF = 0.;                 // Store the local degree of freedom
 };
 
 template<typename data_t>
@@ -41,9 +45,17 @@ struct Ising2d
 {
     State<data_t> current_state;
 
-    float * spin_array = nullptr;
+    data_t * spin_array = nullptr;
     size_t * neighbor_array = nullptr;
 
+    // Add some Hamiltonian dependent functions
+    float local_field(const size_t idx) const;
+    float local_energy(const size_t idx, const data_t spin_value) const;
+    void recalculate_state();
+    void change_state(const size_t idx, State<data_t> & temp_state) const;
+    void set_state(const size_t idx, const State<data_t> & _state);
+
+    // Finally add the constructor and destructor.
     Ising2d()
     {
         // Initialize all spins to 1.
@@ -57,15 +69,71 @@ struct Ising2d
                                             Ising2d_Parameters::L,
                                             Ising2d_Parameters::num_neighbors_i,
                                             neighbor_array);
+
+        recalculate_state();
     }
 
     ~Ising2d()
     {
        if ( spin_array != nullptr ) delete [] spin_array;
        if ( neighbor_array != nullptr ) delete [] neighbor_array;
-    }
+    } 
 };
 
+template<typename data_t>
+float Ising2d<data_t>::local_field(const size_t idx) const
+{
+    float field = Ising2d_Parameters::h;
+    for ( size_t nidx = 0; nidx != Ising2d_Parameters::num_neighbors_i; ++nidx )
+        field += Ising2d_Parameters::J * static_cast<float>( spin_array[ neighbor_array[ idx * Ising2d_Parameters::num_neighbors_i + nidx ] ] );
+
+    return field;
+}
+
+template<typename data_t>
+float Ising2d<data_t>::local_energy(const size_t idx, const data_t spin_value) const
+{
+    return -static_cast<float>( spin_value[idx] ) * local_field(idx);
+}
+
+template<typename data_t>
+void Ising2d<data_t>::recalculate_state()
+{
+    float temp_energy = 0.;
+    data_t temp_magnetization = 0.;
+
+    for ( size_t idx = 0; idx != Ising2d_Parameters::N; ++idx )
+    {
+       temp_magnetization += spin_array[idx];
+       // First term is necessary to account for h field loss
+       // when 0.5 multiplies the energy to avoid double counting.
+       temp_energy += -0.5 * Ising2d_Parameters::h * static_cast<float>(spin_array[idx]) + 0.5 * local_energy(idx, spin_array[idx]);
+    }
+
+    current_state.energy = temp_energy;
+    current_state.magnetization = temp_magnetization;
+}
+
+// Change the state by changing the value of the spin
+// at a particular site idx. Return what the new state
+// would be if the change is accepted (the state is
+// passed by reference to avoid extra copies).
+template<typename data_t>
+void Ising2d<data_t>::change_state(const size_t idx, State<data_t> & temp_state) const
+{
+    temp_state.DoF = -spin_array[idx];
+    temp_state.magnetization = current_state.magnetization + temp_state.DoF - spin_array[idx];
+    temp_state.energy = current_state.energy + local_energy(idx, temp_state.DoF) - local_energy(idx, spin_array[idx]);
+}
+
+// Set the current state
+template<typename data_t>
+void Ising2d<data_t>::set_state(const size_t idx, const State<data_t> & _state)
+{
+    current_state.energy = _state.energy;
+    current_state.magnetization = _state.magnetization;
+    spin_array[idx] = _state.DoF;
+}
 
 #endif
 
