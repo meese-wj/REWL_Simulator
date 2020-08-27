0366cb5bddacd5c5675b15a368a722afd07a66bb
W. Joe Meese <meese022@umn.edu>
2020-08-27 12:28:28 -0500

    Add util a folder for special math functions
    Right now it houses a wrapper around standard library random
    number generators. This is to separate the random number generation
    from the Wang Landau functionality. However, the each walker will
    need its own random number generator because they are not thread
    safe. This may lead to a performance hit for single-node simulations
    with shared memory spaces.
    
    Also the util folder is added to the include path.

---
+++ src/util/random_number_generators.hpp
@@ -0,0 +1,27 @@
+#ifndef RANDOM_NUMBER_GENERATORS
+/* This file is really just a wrapper around the Standard Library 
+ * random number generators. This is to separate the random 
+ * number generator from the Wang Landau functionality -- this 
+ * will be more helpful for the replica exchange part of the 
+ * simulation. It can also be used to try different generators
+ * for performance. */
+#include <cmath>
+#include <random>
+
+template<typename data_t>
+struct random_number_generator
+{
+    const std::uint32_t my_seed;
+    std::mt19937 generator (my_seed);
+    std::uniform_real_distribution<data_t> u_real(0., 1.);
+    // TODO: Should I add a uniform int distribution too? 
+    
+    random_number_generator(const std::uint32_t _seed) : my_seed(_seed) {}
+    ~random_number_generator();
+   
+    data_t operator () () { return u_real(generator); }
+    data_t get_real_modified(const data_t min, const data_t max){ return min + (max - min) * u_real(generator); }
+    
+};
+
+#endif
