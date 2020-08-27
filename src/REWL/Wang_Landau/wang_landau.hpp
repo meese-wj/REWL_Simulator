d23dddf380210c7748f946bf394d3aa64139938d
W. Joe Meese <meese022@umn.edu>
2020-08-27 11:45:16 -0500

    Add Wang_Landau subdirecory for functions
    This subdirectory will hold the relevant functions for a single
    Wang Landau walker.

---
+++ src/REWL/Wang_Landau/wang_landau.hpp
@@ -0,0 +1,11 @@
+#ifndef WANG_LANDAU
+/* Use this file to define the Wang Landau
+ * operations for a single random walker.
+ * These functions will be used by every
+ * MPI process running in the Replica-
+ * Exchange Wang Landau algorithm. */
+
+#include <cmath>
+#include <random_number_generator.hpp>
+
+#endif
