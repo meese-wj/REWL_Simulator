83c5a048ee8736d22a5bf15c8af8301e065f009b
W. Joe Meese <meese022@umn.edu>
2020-09-07 04:43:32 -0500

    Fix the periodic grid initializer
    Was trying to modify a copy of the pointer the neighbor array
    rather than the reference to the pointer. Thus the neighbor array
    remained uninitialized in the constructor. Now that is fixed.

--- src/Hamiltonians/grid/grid_setup.cpp
+++ src/Hamiltonians/grid/grid_setup.cpp
@@ -1,40 +1,43 @@
 // Compile the header in source.
+#include <stdio.h>
 #include "grid_setup.hpp"
 
 void define_2d_square_periodic_neighbors(const size_t Lx,
                                          const size_t Ly,
                                          const size_t num_neighbors,
-                                         size_t * neighbor_array)
+                                         size_t *& neighbor_array)
 {
     neighbor_array = nullptr;
     neighbor_array = new size_t [num_neighbors * Lx * Ly];
 
     size_t neighbor_idx;
     size_t neighbor_jdx;
+    size_t site;
     for ( size_t idx = 0; idx != Lx; ++idx )
     {
         for ( size_t jdx = 0; jdx != Ly; ++jdx )
         {
-            // Neighbor 0: idx - 1, jdx
-            // Neighbor 1: idx + 1, jdx
-            // Neighbor 2: idx    , jdx - 1
-            // Neighbor 3: idx    , jdx + 1
-
-            neighbor_idx = ( idx == 0 ? Lx - 1 : idx - 1 );
-            neighbor_jdx = jdx;
-            neighbor_array[ neighbor_idx * num_neighbors + 0 ] = neighbor_idx * Lx + neighbor_jdx;
-
-            neighbor_idx = ( idx == Lx - 1 ? 0 : idx + 1 );
-            neighbor_jdx = jdx;
-            neighbor_array[ neighbor_idx * num_neighbors + 1 ] = neighbor_idx * Lx + neighbor_jdx;
-
+            // Neighbor 0: idx    , jdx - 1
+            // Neighbor 1: idx    , jdx + 1
+            // Neighbor 2: idx - 1, jdx
+            // Neighbor 3: idx + 1, jdx
+            site = idx * Lx + jdx;
+            
             neighbor_idx = idx;
             neighbor_jdx = ( jdx == 0 ? Ly - 1 : jdx - 1 );
-            neighbor_array[ neighbor_idx * num_neighbors + 2 ] = neighbor_idx * Lx + neighbor_jdx;
+            neighbor_array[ site * num_neighbors + 0 ] = neighbor_idx * Lx + neighbor_jdx;
 
             neighbor_idx = idx;
             neighbor_jdx = ( jdx == Ly - 1 ? 0 : jdx + 1 );
-            neighbor_array[ neighbor_idx * num_neighbors + 3 ] = neighbor_idx * Lx + neighbor_jdx;
+            neighbor_array[ site * num_neighbors + 1 ] = neighbor_idx * Lx + neighbor_jdx;
+
+            neighbor_idx = ( idx == 0 ? Lx - 1 : idx - 1 );
+            neighbor_jdx = jdx;
+            neighbor_array[ site * num_neighbors + 2 ] = neighbor_idx * Lx + neighbor_jdx;
+
+            neighbor_idx = ( idx == Lx - 1 ? 0 : idx + 1 );
+            neighbor_jdx = jdx;
+            neighbor_array[ site * num_neighbors + 3 ] = neighbor_idx * Lx + neighbor_jdx;
 
         }
     }
--- src/Hamiltonians/grid/grid_setup.hpp
+++ src/Hamiltonians/grid/grid_setup.hpp
@@ -4,6 +4,6 @@
 void define_2d_square_periodic_neighbors(const size_t Lx,
                                          const size_t Ly,
                                          const size_t num_neighbors,
-                                         size_t * neighbor_array);
+                                         size_t *& neighbor_array);
 
 #endif
