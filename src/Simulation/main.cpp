a640233b64975426d362669cbe7397fd9f20a17d
W. Joe Meese <meese022@umn.edu>
2020-09-11 12:48:28 -0500

    Need to change final logdos and final observables
    These should be extracted and then fed into the thermodynamics
    code. This will probably be easier. The energies should probably
    be fed into this object too.

--- src/Simulation/main.cpp
+++ src/Simulation/main.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 #include "rewl_simulation.hpp"
+#include <thermodynamics.hpp>
+
+using thermo_t = Thermodynamics<ENERGY_TYPE, LOGDOS_TYPE, OBS_TYPE, System_Obs_enum_t, Observables_t>;
 
 int main(const int argc, const char * argv[])
 {
@@ -18,7 +21,18 @@ int main(const int argc, const char * argv[])
     simulation -> simulate();
     printf("\nEnd of simulation. Exiting.\n\n");
 
+    printf("\nNow calculating canonical thermodynamics.");
+
+    thermo_t * thermo = new thermo_t ( System_Parameters::energy_min, System_Parameters::energy_max, System_Parameters::energy_bin_size, 0.1, 4.7, 1000 );
+    
+    LOGDOS_TYPE * final_logdos_array = nullptr;
+    thermo -> calculate_thermodynamics( System_Parameters::N, final_logdos_array, 
+                                        final_observable_array ); 
+
     delete simulation;
+    delete [] final_logdos_array;
+    delete [] final_observable_array;
+    delete thermo;
 
     return 0;
 }
