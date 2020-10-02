26eda9f2aecbbd1dc94ed7a4e69d48a5da760ce6
W. Joe Meese <meese022@umn.edu>
2020-10-01 20:15:34 -0500

    Add a valgrind script in serial

---
+++ Scripts/run_serial_valgrind.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd /home/joe/Linux_Code_Dev/REWL_Simulator/build_serial
+echo
+pwd
+echo
+ls
+echo
+echo "Building and running code."
+cmake .. -DMPI_ON=OFF
+
+make -j
+if [[ $? == 0 ]]
+then
+    echo
+    echo
+    time valgrind --leak-check=yes ./bin/REWL_Simulator
+else
+    echo
+    echo "Error in compilation. Exiting."
+fi
+echo
+
