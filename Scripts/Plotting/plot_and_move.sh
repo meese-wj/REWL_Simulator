59e168e9808044b35864ee4d81eba558cb556d45
W. Joe Meese <meese022@umn.edu>
2020-10-19 16:27:27 -0500

    Combine plotting and copying into one script

---
+++ Scripts/Plotting/plot_and_move.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+while getopts ":m:d:c:v:T:" arg;
+do
+    case $arg in
+        m) MODEL=$OPTARG;;
+        d) DATE=$OPTARG;;
+        c) COUPLING=$OPTARG;;
+        v) VALUE=$OPTARG;;
+        T) TC_VALUE=$OPTARG;;  # Optional Tc for vertical line
+    esac
+done
+
+./Scripts/Plotting/plot_model_sizes_for_fixed_coupling.sh -m $MODEL -d $DATE -c $COUPLING -v $VALUE -T $TC_VALUE
+
+if [ $? == 0 ]
+then
+    ./Scripts/Plotting/move_data_to_windows.sh -m $MODEL -d $DATE -c $COUPLING -v $VALUE
+fi
+
+
