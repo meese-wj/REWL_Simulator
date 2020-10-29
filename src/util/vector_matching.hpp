c6bcc14abbccebf6e03d355817170272aa452c6a
W. Joe Meese <meese022@umn.edu>
2020-10-28 21:23:40 -0500

    Add new vector matching function to two arbitrary logdos

--- src/util/vector_matching.hpp
+++ src/util/vector_matching.hpp
@@ -27,6 +27,60 @@ void find_left_indices( std::vector<size_t> & left_indices, const std::vector<st
     }
 }
 
+struct index_pairs
+{
+    size_t left = 0;
+    size_t right = 0;
+};
+
+// Find the indices corresponding to the 
+// first overlap of two vectors
+template<typename energy_t, typename logdos_t>
+void find_left_concatenation_indices( index_pair * const concatenate_indices,
+                                      index_pair * const start_indices, 
+                                      const std::vector<logdos_t> & logdos_left,
+                                      const std::vector<logdos_t> & logdos_right,
+                                      const std::vector<energy_t> & energy_left,
+                                      const std::vector<energy_t> & energy_right )
+{
+    size_t num_bins = logdos_left.size();
+    
+    // Find the first index of the overlap
+    energy_t right_energy_val = energy_right.front();
+    size_t left_start_index = INT32_MAX;
+    for ( int ldx = energy_left.size() - 1; ldx != -1; --ldx )
+        left_start_index = ( energy_left[ static_cast<size_t>(ldx) ] == right_energy_val ? static_cast<size_t>(ldx) : INT32_MAX );
+
+    if ( left_start_index == INT32_MAX )
+    {
+        printf("\nThere is no overlap. This is a huge problem, so I'm gonna exit in a leaky way.\n");
+        exit(1);
+    }
+
+    start_indices -> left  = left_start_index;
+    start_indices -> right = 0; 
+    
+    // Now find the index representing the lowest
+    // difference in derivative
+    logdos_t left_beta  = derivative_of_vector( start_indices -> left, logdos_left, energy_left );
+    logdos_t right_beta = derivative_of_vector( start_indices -> right, logdos_right, energy_right );
+    logdos_t beta_diff  = abs(left_beta - right_beta);
+    concatenate_indices -> left  = start_indices -> left; 
+    concatenate_indices -> right = start_indices -> right; 
+    for ( size_t idx = 1; idx != num_bins; ++idx )
+    {
+        left_beta  = derivative_of_vector( start_indices -> left  + idx,  logdos_left,  energy_left );
+        right_beta = derivative_of_vector( start_indices -> right + idx, logdos_right, energy_right );
+        if ( (abs(left_beta - right_beta) + 0.5) < (beta_diff + 0.5) )
+        {
+            beta_diff = abs(left_beta - right_beta);
+            concatenate_indices -> left  = start_indices -> left  + idx;
+            concatenate_indices -> right = start_indices -> right + idx; 
+        }
+    }
+
+}
+
 // Find the indices of the concatenation
 // in the leftward energy windows via
 // numerical differentiation of the 
