Averaging is correct for num_jobs = 1
So hopefully it holds for arbitarary num_jobs. Will need to work
on nonlinear observables moving forward. It might just have to be
a model-by-model thing...

Scripts/Plotting/parse_and_plot_observables.py
@@ -206,16 +206,16 @@ def plot_data_tuples( model_name, data_file_stem, coupling_string, coupling_valu
         print("\nPlotting %s vs %s" % (labels[lbl], xlabel))
         fig, ax = plt.subplots(1,1)
 
-        epsilon_range = 0.025
+        epsilon_range = None
         xmin, xmax, plt_ymin, plt_ymax = 0, 0, 0, 0
-        if Tc_val != None and Tc_val != "":
+        if epsilon_range != None and Tc_val != None and Tc_val != "":
             xmin, xmax = (1 - epsilon_range) * float(Tc_val), (1 + epsilon_range) * float(Tc_val)
 
         for Ldx in range(0, len(data_tuples)):
 
             Lvalue = data_tuples[Ldx][0]
             ax.plot(data_tuples[Ldx][1][:,0], data_tuples[Ldx][1][:,lbl], label = r"$L = %s$" % Lvalue)
-            if Tc_val != None and Tc_val != "":
+            if epsilon_range != None and Tc_val != None and Tc_val != "":
                 if "microcanonical" not in data_file_stem and "Counts" not in labels[lbl]:
                     test_ymin, test_ymax = get_y_range( data_tuples[Ldx][1][:,lbl], data_tuples[Ldx][1][:,0], xmin, xmax )
                     if abs(test_ymin) > abs(plt_ymin):
@@ -223,14 +223,15 @@ def plot_data_tuples( model_name, data_file_stem, coupling_string, coupling_valu
                     if abs(test_ymax) > abs(plt_ymax):
                         plt_ymax = test_ymax
 
-        if Tc_val != None and Tc_val != "":
+        if epsilon_range != None and Tc_val != None and Tc_val != "":
             if "microcanonical" not in data_file_stem and "Counts" not in labels[lbl]:
                 ax.set_ylim([plt_ymin, plt_ymax])
 
         if Tc_val != None and Tc_val != "":
             if "microcanonical" not in data_file_stem:
                 ax.set_xlim([0, 2 * float(Tc_val)])
-                ax.set_xlim([xmin, xmax])
+                if epsilon_range != None:
+                    ax.set_xlim([xmin, xmax])
 
                 ymin, ymax = ax.get_ylim()
                 ax.plot( float(Tc_val) + 0. * np.linspace(0,1,10), ymin + (ymax - ymin) * np.linspace(0,1,10), color = "gray", lw = 1, ls = "dashed", label = r"$T_c = %s$" % Tc_val )

Statistics/average_canonical_obs.py
@@ -90,7 +90,7 @@ def read_in_data( input_path, Lsize, coupling_symbol, coupling_value, comment = 
 # and energy2.
 def specific_heat( energy2, energy, size, temp ):
 
-    return ( energy2 - size * energy * energy ) / ( temp * temp )
+    return ( energy2 - size * energy * energy ) / ( temp ** 2. )
 
 # Calculate error for specific heat from
 # intensive energy
@@ -120,7 +120,7 @@ def average_job_data( Lsize, labels, data_tuples ):
     en2_idx = labels.index("Energy2")
     cv_idx = labels.index("Specific Heat")
 
-    final_data[:, cv_idx] = specific_heat( final_data[:, en_idx], final_data[:, en2_idx], Nfloat, final_data[:, 0] )
+    final_data[:, cv_idx] = specific_heat( final_data[:, en2_idx], final_data[:, en_idx], Nfloat, final_data[:, 0] )
 
     return final_data
 
@@ -130,13 +130,19 @@ def stderr_job_data( Lsize, labels, data_tuples, final_averages ):
 
     num_jobs = len(data_tuples)
 
+    divisor = num_jobs
+    if num_jobs == 1:
+        divisor *= num_jobs
+    else:
+        divisor *= (num_jobs - 1)
+
     final_stderr = np.zeros(final_averages.shape)
 
     # Get Standard Error along columns
     for col in range(0, len(labels)):
         for job in range(0, num_jobs):
             final_stderr[:,col] += ( data_tuples[job][1][:,col] - final_averages[:,col] ) ** 2.
-        final_stderr[:,col] = np.sqrt( final_stderr[:,col] / (num_jobs * (num_jobs)) )
+        final_stderr[:,col] = np.sqrt( final_stderr[:,col] / divisor )
 
     # Recompute the specific heat error
     # TODO: This will break for higher dimensions
@@ -161,9 +167,12 @@ def write_out_data( output_file, input_path, num_jobs, header_lines, final_avera
 
     header = ""
     for ldx in range(0, len(header_lines)):
-        header += header_lines[ldx]
+        header += header_lines[ldx].rstrip()
+        if ldx != len(header_lines)-1:
+            header += "\n"
 
-    print(header)
+    np.savetxt(output + ".job_mean", final_averages, delimiter="  ", newline="\n", header=header, comments="")
+    np.savetxt(output + ".job_stderr", final_stderr, delimiter="  ", newline="\n", header=header, comments="")
 
     return None
 
@@ -182,7 +191,7 @@ def main():
 
     final_stderr = stderr_job_data( cl_args.Lsize, labels, data_tuples, final_averages )
 
-    write_out_data( output_file, cl_args.input_path, len(data_tuples), header_lines, final_averages, final_averages )
+    write_out_data( output_file, cl_args.input_path, len(data_tuples), header_lines, final_averages, final_stderr )
 
     return 0
 