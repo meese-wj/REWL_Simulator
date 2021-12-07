0985fd3ab38776ecef1192a3dcbbcb82ba05f9ba
W. Joe Meese <meese022@umn.edu>
2021-12-07 10:46:12 -0600

    Bugs in the PBC vector functions. Searching now

--- src/Hamiltonians/Ising2d/PM_Dipolar_Interactions/pmd_interactions.hpp
+++ src/Hamiltonians/Ising2d/PM_Dipolar_Interactions/pmd_interactions.hpp
@@ -2,6 +2,9 @@
 #define PMD_INTERACTIONS
 
 #include <cstdint>
+#include <iostream>
+#include <string>
+#include <mpi.h>
 #include "../../grid/pbc_2d_vectors.hpp"
 
 // Include helper functions for 
@@ -22,9 +25,23 @@ energy_t interaction_value( const pbc_2d_vector<energy_t> & site_1, const pbc_2d
     if (site_1.x == site_2.x && site_1.y == site_2.y)
         return 0.;
 
+    int my_id = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+
     energy_t xprojection = difference.x;
     energy_t cos_theta = xprojection / magnitude<energy_t, energy_t>( difference );
     energy_t cos_theta_sq = cos_theta * cos_theta;
+
+    if ( site_1.x == 0. && site_1.y && my_id == 0 )
+    {
+        std::cout << "\n";
+        std::cout << "\nsite_2      = (" << (int)site_2.x << ", " << (int)site_2.y << ")";
+        std::cout << "\ndiff        = (" << (int)difference.x << ", " << (int)difference.y << ")";
+        std::cout << "\ncos(theta)  = " << cos_theta;
+        std::cout << "\ncos(4theta)  = " << 1. - 8. * cos_theta_sq + 8. * cos_theta_sq * cos_theta_sq;
+        std::cout << "\n";
+    }
+
     return (1. - 8. * cos_theta_sq + 8. * cos_theta_sq * cos_theta_sq) / square_magnitude<energy_t, energy_t>( difference );
 }
 
@@ -61,26 +78,35 @@ template<typename energy_t, typename spin_t>
 void PMDN_Interactions<energy_t, spin_t>::build_interaction_matrix(const energy_t phonon_coupling, const std::uint32_t Lx, const std::uint32_t Ly)
 {
     std::uint32_t system_size = Lx * Ly;
+    std::string printer = "\n";
     interaction_matrix = new energy_t [ system_size * system_size ]();
+    int my_id = 0;
+    MPI_Comm_rank( MPI_COMM_WORLD, &my_id );
     for ( std::uint32_t x1 = 0; x1 != Lx; ++x1 )
         for ( std::uint32_t y1 = 0; y1 != Ly; ++y1 )
         {
             std::uint32_t index_1 = x1 * Lx + y1;
             pbc_2d_vector<energy_t> site_1( x1, y1 );
             
+            printer += std::to_string( index_1 ) + ":  ";
             for ( std::uint32_t x2 = 0; x2 != Lx; ++x2 )
                 for ( std::uint32_t y2 = 0; y2 != Ly; ++y2 )
                 {    
                     std::uint32_t index_2 = x2 * Lx + y2;
                     pbc_2d_vector<energy_t> site_2( x2, y2 );
-
+                    if (my_id == 0 && (x1 == 0 && y1 == 0))
+                        std::cout << "\n(" << x2 << ", " << y2 << ") -> " << index_2 << "\n"; 
                     interaction_matrix[ index_1 * total_sites + index_2 ] = 0.;
                     if ( !( x1 == x2 && y1 == y2 ) )
                         interaction_matrix[ index_1 * total_sites + index_2 ] = phonon_coupling * interaction_value( site_1, site_2, Lx, Ly );
-                        
+                    printer += std::to_string( interaction_matrix[ index_1 * total_sites + index_2 ] / phonon_coupling ) + " ";
                 }
+            printer += "\n";
         }
-    
+
+    printer += "\n";
+    if ( my_id == 0 )
+        std::cout << printer;
     return;
 }
 
@@ -92,7 +118,7 @@ energy_t PMDN_Interactions<energy_t, spin_t>::calculate_energy_per_spin( const s
     for (std::uint32_t idx = 0; idx != total_sites; ++idx)
     {
         // The interaction strength of (site, site) is defined as zero
-        // to not accidentally introduce branches as well as this behemoth...
+        // to not accidentally introduce code branches into this behemoth...
         summand += interaction_strength( site, idx ) * static_cast<energy_t>( all_spins[idx] );
     }
     return static_cast<energy_t>(spin_val) * summand;    
@@ -122,6 +148,7 @@ energy_t PMNI_ground_state_contribution( const energy_t phonon_coupling, const s
         ferromagnetic_ground_state_spins[site] = moment_value;
 
     energy_t total_energy = pmd_ints.calculate_total_PMDN_energy( ferromagnetic_ground_state_spins );
+    std::cout << "Total energy = " << total_energy << "\n";
     delete [] ferromagnetic_ground_state_spins;
     return total_energy;
 }
