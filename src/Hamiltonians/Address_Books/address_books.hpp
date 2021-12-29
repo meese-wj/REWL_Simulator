687d66f284312bf2a0795f4d324b78590e0bbb33
W. Joe Meese <meese022@umn.edu>
2021-12-29 16:12:26 -0600

    Move all the address book includes into a single file

---
+++ src/Hamiltonians/Address_Books/address_books.hpp
@@ -0,0 +1,12 @@
+#ifndef _ADDRESS_BOOKS_H
+#define _ADDRESS_BOOKS_H
+
+// This header should be included for all
+// Hamiltonians, just to make everything 
+// easier. It contains all of the headers
+// of the subsequent non-test folders.
+
+#include "Square_Periodic_Lattices/square_2d_nearest_neighbors.hpp"
+#include "Square_Periodic_Lattices/square_2d_nearest_neighbor_functor.cpp"  // This source file needs to be included to avoid linker errors!
+
+#endif /* _ADDRESS_BOOKS_H */
