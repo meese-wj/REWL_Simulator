8771300ec0a34986934cdff6086d0965c7d08974
W. Joe Meese <meese022@umn.edu>
2022-02-09 19:00:45 -0600

    Revert "Working on possible optimizations with constexpr function pointers"
    This reverts commit fc68e86e2c1e4d807dd351159eb67bcfe4253dde.

--- src/Hamiltonians/Address_Books/Square_Periodic_Lattices/square_2d_grid_helpers.hpp
+++ src/Hamiltonians/Address_Books/Square_Periodic_Lattices/square_2d_grid_helpers.hpp
@@ -2,7 +2,6 @@
 #define SQUARE_2D_GRID_HELPERS_H
 
 #include <cstdint>
-#include <helpful_global_macros.hpp>   // included from util
 
 /* All functions are based on the flattened mapping:
    
@@ -30,43 +29,6 @@ constexpr std::uint32_t site_x_index( std::uint32_t site ) { return SITE_X_FUNCT
 
 template<std::uint32_t Lx>
 constexpr std::uint32_t site_y_index( std::uint32_t site ) { return SITE_Y_FUNCTION(site, Lx); }
-
-// PBC functions for a 2D square grid when Lx and Ly are known at compile time
-// Zero neighbor (x - 1, y)
-template<std::uint32_t Lx, std::uint32_t Ly = Lx>
-constexpr std::uint32_t rect_pbc_2d_neighbor_0( std::uint32_t site_x, std::uint32_t site_y )
-{
-   std::uint32_t neighbor_x = _BRANCHLESS_TERNARY( site_x == 0, Lx - 1, site_x - 1 );
-   std::uint32_t neighbor_y = site_y;
-   return site_from_indices<Lx>( neighbor_x, neighbor_y );
-}
-
-// One neighbor (x + 1, y)
-template<std::uint32_t Lx, std::uint32_t Ly = Lx>
-constexpr std::uint32_t rect_pbc_2d_neighbor_1( std::uint32_t site_x, std::uint32_t site_y )
-{
-   std::uint32_t neighbor_x = _BRANCHLESS_TERNARY( site_x == Lx - 1, 0, site_x + 1 );
-   std::uint32_t neighbor_y = site_y;
-   return site_from_indices<Lx>( neighbor_x, neighbor_y );
-}
-
-// Two neighbor (x, y - 1)
-template<std::uint32_t Lx, std::uint32_t Ly = Lx>
-constexpr std::uint32_t rect_pbc_2d_neighbor_2( std::uint32_t site_x, std::uint32_t site_y )
-{
-   std::uint32_t neighbor_x = site_x;
-   std::uint32_t neighbor_y = _BRANCHLESS_TERNARY( site_y == 0, Ly - 1, site_y - 1 );
-   return site_from_indices<Lx>( neighbor_x, neighbor_y );
-}
-
-// Three neighbor (x, y + 1)
-template<std::uint32_t Lx, std::uint32_t Ly = Lx>
-constexpr std::uint32_t rect_pbc_2d_neighbor_3( std::uint32_t site_x, std::uint32_t site_y )
-{
-   std::uint32_t neighbor_x = site_x;
-   std::uint32_t neighbor_y = _BRANCHLESS_TERNARY( site_y == Ly - 1, 0, site_y + 1 );
-   return site_from_indices<Lx>( neighbor_x, neighbor_y );
-}
 
 #undef SITE_FUNCTION
 #undef SITE_X_FUNCTION
