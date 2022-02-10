6375da93878a24931d0d25377b26bdd1fcf0edfa
W. Joe Meese <meese022@umn.edu>
2022-02-09 22:49:22 -0600

    WIP: Adding Ising sites and the container for it
    Deciding what to do about cases of zero or constant field. Maybe
    just make the container smart?

---
+++ src/Hamiltonians/Ising2d/ising_site.hpp
@@ -0,0 +1,30 @@
+#ifndef _ISING_SITE_H
+#define _ISING_SITE_H
+// Use this to define a site for the Ising model.
+#include "../Site_Variables/site_variables.hpp"
+
+template<typename data_t>
+class Ising_Site : public Site_Variables
+{
+public:
+    enum variable_enum 
+    {
+        spin=0, field, NUM_VARIABLES
+    };
+    
+    void set_variable( const int which_var, const data_t var_val ) const { _variables[which_var] = var_val; }
+    const data_t & get_variable( const int which_var ) const { return _variables[which_var]; }
+
+    Ising_Site( ) {};
+    Ising_Site( const data_t spin_val, const data_t field_val )
+    {
+        set_variable(variable_enum::spin,  spin_val);
+        set_variable(variable_enum::field, field_val);
+    }
+
+    virtual ~Ising_Site();
+private:
+    data_t _variables [ NUM_VARIABLES ] = { 0., 0. };
+}
+
+#endif /* _ISING_SITE_H */
---
+++ src/Hamiltonians/Ising2d/ising_site_container.hpp
@@ -0,0 +1,8 @@
+#ifndef _ISING_SITE_CONTAINER_H
+#define _ISING_SITE_CONTAINER_H
+// Use this object to house a AoS
+// data structure of the Ising Sites.
+
+
+
+#endif /* _ISING_SITE_CONTAINER_H */
